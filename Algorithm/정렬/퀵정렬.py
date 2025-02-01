c9f973f5c9cec1be315976f51570a0f8c359c244
이정현 <leejh@innodep.com>
2025-02-01 19:57:54 +0900

    quick sort

--- Algorithm/정렬/퀵정렬.py
+++ Algorithm/정렬/퀵정렬.py
@@ -1,35 +1,17 @@
-# 삽입 정렬 (O(N^2))
-from collections import deque
-
 def quick_sort(array):
-    left_array = []
-    right_array = []
-    pivot = 0
-    maxindex_array = deque()
-    minindex_array = deque()
-    for i in range(1,len(array)):
-        if array[i]>array[pivot]:
-            maxindex_array.append(i)
-        if array[-i]<array[pivot]:
-            minindex_array.append(-i)
-        if maxindex_array and minindex_array:
-            max_index = maxindex_array.popleft()
-            min_index = minindex_array.popleft()
-            if max_index>-min_index:
-                array[pivot],array[min_index] = array[min_index],array[pivot]
-                answer = []
-                answer.append(array[min_index])
-                print(answer)
-                left_array = array[:min_index]
-                right_array = array[min_index+1:]
-                break
-            else:
-                array[max_index],array[min_index] = array[min_index], array[max_index]
+    if len(array)<=1:
+        return array
+    pivot = array[0]
+    small_list = []
+    big_list = []
+    for i in array[1:]:
+        if i <=pivot:
+            small_list.append(i)
+        if i >pivot:
+            big_list.append(i)
+    
+    return quick_sort(small_list) + [pivot] + quick_sort(big_list)
 
-    quick_sort(left_array)
-    quick_sort(right_array)
-        
-    return unsorted_array
 
 
 
